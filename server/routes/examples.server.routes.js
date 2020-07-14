99b321c74cd0e950990f7290601973b9268ac543
mohitsinghnegi1 <37727115+mohitsinghnegi1@users.noreply.github.com>
2020-07-15 00:46:39 +0530

    added some logic to sort a list of movie on the bases of end_date

--- server/routes/examples.server.routes.js
+++ server/routes/examples.server.routes.js
@@ -1,8 +1,7 @@
-const examples = require('../controllers/examples.server.controller.js'),
-    express = require('express'), 
-    router = express.Router()
+const controllers = require('../controllers/examples.server.controller.js'),
+  express = require('express'),
+  router = express.Router();
 
-router.route('/')
-  .get(examples.hello);
-  
+router.route('/').get(controllers.SendProfitableMovieList);
+
 module.exports = router;
